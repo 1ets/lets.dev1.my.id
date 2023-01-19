93c3ec73f3e4aa803b793bf61e7896a80e913bdc
Dhuta P <dhuta_venturo@loyalto.id>
2023-01-19 14:54:08 +0700

    FIX colors

--- resources/views/marketing.blade.php
+++ resources/views/marketing.blade.php
@@ -141,8 +141,8 @@
       <div class="mt-14 grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
         <div>
           <svg class="w-8 h-8 text-red-500" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path stroke="#F9322C" d="M.5.5h31v31H.5z" />
-            <path d="M8 11v10c0 2.21 3.582 4 8 4s8-1.79 8-4V11M8 11c0 2.21 3.582 4 8 4s8-1.79 8-4M8 11c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" stroke="#F9322C" stroke-linecap="round" stroke-linejoin="round" />
+            <path stroke="#00ADD8" d="M.5.5h31v31H.5z" />
+            <path d="M8 11v10c0 2.21 3.582 4 8 4s8-1.79 8-4V11M8 11c0 2.21 3.582 4 8 4s8-1.79 8-4M8 11c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" stroke="#00ADD8" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
           <h3 class="mt-5 text-xl font-bold">Database</h3>
           <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lets's <a class="underline" href="/docs/eloquent">"Eloquent" ORM</a> provides best-in-class database abstraction without the headaches. Query and update your data without breaking a sweat. Eloquent pairs perfectly with MySQL, Postgres, SQLite, and SQL Server.</p>
@@ -157,16 +157,16 @@
         </div>
         <div>
           <svg class="w-8 h-8 text-red-500" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path stroke="#F9322C" d="M.5.5h31v31H.5z" />
-            <path d="m10.171 15.828-2.12 2.122M19 19l-2 5-4-11 11 4-5 2Zm0 0 5 5-5-5ZM11.188 6.239l.777 2.897-.777-2.897Zm-2.052 5.726-2.898-.777 2.898.777ZM17.95 8.05l-2.122 2.122L17.95 8.05Z" stroke="#F9322C" stroke-linecap="round" stroke-linejoin="round" />
+            <path stroke="#00ADD8" d="M.5.5h31v31H.5z" />
+            <path d="m10.171 15.828-2.12 2.122M19 19l-2 5-4-11 11 4-5 2Zm0 0 5 5-5-5ZM11.188 6.239l.777 2.897-.777-2.897Zm-2.052 5.726-2.898-.777 2.898.777ZM17.95 8.05l-2.122 2.122L17.95 8.05Z" stroke="#00ADD8" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
           <h3 class="mt-5 text-xl font-bold">WebSockets</h3>
           <p class="mt-4 text-gray-700 text-sm leading-relaxed"><a class="underline" href="/docs/broadcasting">Lets Echo</a> and event broadcasting make it a cinch to build modern, realtime user experiences. Create amazing realtime applications while powering your WebSockets with pure PHP, Node.js, or serverless solutions like <a class="underline" href="https://pusher.com">Pusher</a> and <a class="underline" href="https://ably.com">Ably</a>.</p>
         </div>
         <div>
           <svg class="w-8 h-8 text-red-500" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path stroke="#F9322C" d="M.5.5h31v31H.5z" />
-            <path d="M19 11a2 2 0 0 1 2 2l-2-2Zm6 2a6 6 0 0 1-7.743 5.743L15 21h-2v2h-2v2H8a1 1 0 0 1-1-1v-2.586a1 1 0 0 1 .293-.707l5.964-5.964A6 6 0 1 1 25 13v0Z" stroke="#F9322C" stroke-linecap="round" stroke-linejoin="round" />
+            <path stroke="#00ADD8" d="M.5.5h31v31H.5z" />
+            <path d="M19 11a2 2 0 0 1 2 2l-2-2Zm6 2a6 6 0 0 1-7.743 5.743L15 21h-2v2h-2v2H8a1 1 0 0 1-1-1v-2.586a1 1 0 0 1 .293-.707l5.964-5.964A6 6 0 1 1 25 13v0Z" stroke="#00ADD8" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
           <h3 class="mt-5 text-xl font-bold">Authentication</h3>
           <p class="mt-4 text-gray-700 text-sm leading-relaxed">Stop sweating authentication. Lets provides scaffolding for secure, session based authentication, while <a class="underline" href="/docs/sanctum">Lets Sanctum</a> provides painless authentication for APIs and mobile applications.</p>
